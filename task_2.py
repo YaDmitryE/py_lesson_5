20246456d75cb129655217deb67bd9811ef9723b
YaDmitryE <120764954+YaDmitryE@users.noreply.github.com>
2023-09-02 09:40:06 +0300

    1

---
+++ task_2.py
@@ -0,0 +1,20 @@
+# Введите слово с помощью input()
+word = input("Введите слово из маленьких латинских букв: ")
+
+# Создайте переменные для подсчета количества гласных и согласных букв
+vowels = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+consonants = 0
+
+# Переберите буквы в слове и определите, является ли она гласной или согласной
+for letter in word:
+    if letter in vowels:
+        vowels[letter] += 1
+    elif letter.isalpha():
+        consonants += 1
+
+# Проверьте, есть ли все гласные буквы в слове
+if all(vowels.values()):
+    print(f"Гласные: {', '.join([f'{k}: {v}' for k, v in vowels.items()])}")
+    print(f"Согласные: {consonants}")
+else:
+    print(False)
